3e3eb5957f1c93ec80741d2f83e426ead996e894
Fáusio <luisfausio@gmail.com>
2021-08-17 16:15:21 +0200

    Course.Library

--- Course.Library.API/Entities/Author.cs
+++ Course.Library.API/Entities/Author.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace CourseLibrary.API.Entities
+namespace Course.Library.API.Entities
 {
     public class Author
     {
--- Course.Library.API/Entities/Course.cs
+++ Course.Library.API/Entities/Course.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace CourseLibrary.API.Entities
+namespace Course.Library.API.Entities
 {
     public class Course
     {
