25095a998fe064cf2c5d1363650875ab53558686
Fáusio <luisfausio@gmail.com>
2021-08-17 17:07:29 +0200

    CourseController

---
+++ Course.Library.API/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Library.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _repository;
+        private readonly IMapper _mapper;
+        public CourseController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _repository = courseLibraryRepository ?? throw new ArgumentException(nameof(courseLibraryRepository));
+        }
+    }
+}
