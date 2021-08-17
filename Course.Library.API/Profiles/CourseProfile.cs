0507e7370f44d63ad602eaaa20baf51f99197b5b
Fáusio <luisfausio@gmail.com>
2021-08-17 17:16:17 +0200

    CourseProfile

---
+++ Course.Library.API/Profiles/CourseProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Course.Library.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Library.API.Profiles
+{
+    public class CourseProfile : Profile
+    {
+        public CourseProfile()
+        {
+            CreateMap<Entities.Course, CourseDto>(); 
+        }
+    }
+}
