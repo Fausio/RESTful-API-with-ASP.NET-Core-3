1586df62b23bf3c7778ef38e7af9428474ff9aa2
Fáusio <luisfausio@gmail.com>
2021-08-17 16:14:51 +0200

    AuthorDto

---
+++ Course.Library.API/Models/AuthorDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Library.API.Models
+{
+    public class AuthorDto
+    {
+      
+        public Guid Id { get; set; }
+          
+        public string FirstName { get; set; }
+         
+        public string LastName { get; set; }
+         
+        public DateTimeOffset DateOfBirth { get; set; }
+         
+        public string MainCategory { get; set; }
+         
+    }
+}
